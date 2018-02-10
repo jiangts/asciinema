f07b59a7e605ba2edc47315ce86f5ea4563ba8f6
Allan Jiang <origami789@gmail.com>
2018-02-09 19:00:23 -0800

    add docs

--- motif.py
+++ motif.py
@@ -1,2 +1,8 @@
 from asciinema.__main__ import main
 main()
+
+# require asciinema like above, so you can pyinstaller a single .py file
+# then, from this link
+# https://stackoverflow.com/questions/15114695/pyinstaller-import-error
+# I found that you need to not only declare requests as a hidden module,
+# but add to pathex, which is basically the classpath
