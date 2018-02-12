3dd9cd7ebb8b71e25432317d7963f51c6d7ca730
Allan Jiang <origami789@gmail.com>
2018-02-12 01:40:10 -0800

    motif setup script

--- asciinema/__init__.py
+++ asciinema/__init__.py
@@ -1,7 +1,7 @@
 import sys
 
-__author__ = 'Marcin Kulik'
-__version__ = '1.4.0'
+__author__ = 'Allan Jiang'
+__version__ = '1.4.1'
 
 if sys.version_info[0] < 3:
     raise ImportError('Python < 3 is unsupported.')
